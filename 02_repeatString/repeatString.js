685d6e51c2452c4b9fa1f9c12ef408e318a97ebf
rodrigomacedo-sjr <rodrigomacedo.sjr@gmail.com>
2025-03-20 21:22:36 -0300

    solved first two exercises!

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (myStr, quant) {
+  if (quant < 0) return "ERROR"
+  ret = "";
+  for (let i = 0; i < quant; i++) {
+    ret += String(myStr);
+  }
+  return ret;
 };
 
 // Do not edit below this line
