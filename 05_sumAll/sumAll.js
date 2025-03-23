6a7d4580fa963e9e2ca150132cc23763f50dcd0e
rodrigomacedo-sjr <rodrigomacedo.sjr@gmail.com>
2025-03-23 09:41:44 -0300

    solved exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+  if (typeof a != "number" || typeof b != "number") return "ERROR";
+  if (a % 1 != 0 || b % 1 != 0) return "ERROR";
+  if (a < 0 || b < 0) return "ERROR";
 
+  if (b < a) {
+    a = a ^ b;
+    b = a ^ b;
+    a = a ^ b;
+  }
+
+  sum = 0;
+  for (let i = a; i <= b; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
