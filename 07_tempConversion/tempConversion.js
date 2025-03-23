9f13f0ca10bba24fbdd1919c74bf5b36e242f61f
rodrigomacedo-sjr <rodrigomacedo.sjr@gmail.com>
2025-03-23 09:56:11 -0300

    solved exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  if (typeof num != "number") return "ERROR";
+  num = ((num - 32) * 5) / 9;
+  return Math.round(num * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  if (typeof num != "number") return "ERROR";
+  num = (num * 9) / 5 + 32;
+  return Math.round(num * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
