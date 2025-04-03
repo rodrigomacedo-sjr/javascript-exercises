a5ad016435a3b004d4b61384f1519905744cac26
rodrigomacedo-sjr <rodrigomacedo.sjr@gmail.com>
2025-04-03 17:31:48 -0300

    solved exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (a, b) {
+  if (Array.isArray(a)) {
+    let sum = 0;
+    for (x of a) sum += x;
+    return sum;
+  }
+
+  return a + b;
 };
 
-const multiply = function() {
+const multiply = function (a, b) {
+  if (Array.isArray(a)) {
+    let res = 1;
+    for (x of a) res *= x;
+    return res;
+  }
 
+  a * b;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  let res = 1;
+  for (let i = 0; i < b; i++) {
+    res *= a;
+  }
+  return res;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  if (a <= 1) return 1;
+  if (a == 2) return 2;
+  return a * factorial(a - 1);
 };
 
 // Do not edit below this line
@@ -29,5 +47,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
