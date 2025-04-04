3854a610580e919f7652794106e8f4e20363d0af
rodrigomacedo-sjr <rodrigomacedo.sjr@gmail.com>
2025-04-04 13:34:32 -0300

    solved exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  return people.sort((a, b) => {
+    if (a["yearOfDeath"] == undefined) {
+      a["yearOfDeath"] = new Date().getFullYear();
+    }
+    if (b["yearOfDeath"] == undefined) {
+      b["yearOfDeath"] = new Date().getFullYear();
+    }
 
+    let age1 = a.yearOfDeath - a.yearOfBirth;
+    let age2 = b.yearOfDeath - b.yearOfBirth;
+
+    if (age1 == age2) return 0;
+    if (age1 > age2) return -1;
+    if (age1 < age2) return 1;
+  })[0];
 };
 
 // Do not edit below this line
