61709255b1346f6b9a84208c9c6b7ae57d2e3d2d
rodrigomacedo-sjr <rodrigomacedo.sjr@gmail.com>
2025-04-04 08:57:49 -0300

    solved exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
-
+const palindromes = function(words) {
+  let arr = words.split("");
+  let len = words.length;
+  for (let i = 0; i < len; i++) {
+    if (!isLetter(arr[i])) {
+      arr.splice(i, 1);
+      len--;
+      i--;
+    }
+  }
+  words = arr.join("").toUpperCase();
+  let sdrow = arr.reverse().join("").toUpperCase();
+  return words == sdrow;
 };
 
+function isLetter(str) {
+  return str.length === 1 && str.match(/[a-z0-9]/i);
+}
 // Do not edit below this line
 module.exports = palindromes;
