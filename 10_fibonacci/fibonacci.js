4444b3ad61b92a7c8b5a99b6a39d2982d7809614
rodrigomacedo-sjr <rodrigomacedo.sjr@gmail.com>
2025-04-04 09:24:08 -0300

    solved exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,9 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+  a = Number(a);
+  if (a < 0) return "OOPS";
+  else if (a == 0) return 0;
+  else if (a <= 2) return 1;
+  else return fibonacci(a - 1) + fibonacci(a - 2);
 };
 
 // Do not edit below this line
